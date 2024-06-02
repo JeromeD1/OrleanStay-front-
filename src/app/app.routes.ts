4b789a7d5b2d5ecb3737fd5a38e53589b312e507
Jerome Desouches <jerome.desouches@gmail.com>
2024-06-02 15:04:48 +0200

    creation des composants (vide) de la partie admin + la navbar admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TemplateEmailComponent } from './components/template-email/template-email.component';
+import { AdminInterfaceComponent } from './pages/admin-interface/admin-interface.component';
+import { AcceptReservationComponent } from './components/accept-reservation/accept-reservation.component';
+import { AddReservationComponent } from './components/add-reservation/add-reservation.component';
+import { AppartmentGestionComponent } from './components/appartment-gestion/appartment-gestion.component';
 
 export const routes: Routes = 
 [
     {path:"", component: HomeComponent},
     {path: "templateEmail/:appartmentId", component: TemplateEmailComponent},
+    {path: "admin", component: AdminInterfaceComponent, children:
+        [
+            {path:"acceptReservation", component: AcceptReservationComponent},
+            {path:"", redirectTo:"acceptReservation", pathMatch: 'full'},
+            {path: "addReservation", component: AddReservationComponent},
+            {path: "appartGestion", component: AppartmentGestionComponent}
+        ]
+    },
 ];
