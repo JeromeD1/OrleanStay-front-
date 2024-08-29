af43c3e02818a4089923aedd7d33906070427f75
Jerome Desouches <jerome.desouches@gmail.com>
2024-08-29 18:05:46 +0200

    tableau des comptes

--- src/app/components/statistiques-resa/statistiques-resa.component.ts
+++ src/app/components/statistiques-resa/statistiques-resa.component.ts
@@ -7,11 +7,13 @@ import { User } from '../../models/User.model';
 import { Router } from '@angular/router';
 import { take } from 'rxjs';
 import { AppartmentBusinessStat } from '../../models/AppartmentBusinessStat.model';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-statistiques-resa',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './statistiques-resa.component.html',
   styleUrl: './statistiques-resa.component.scss'
 })
@@ -23,19 +25,20 @@ export class StatistiquesResaComponent implements OnInit {
   currentUser: User | null = this.appstore.getCurrentUser()()
   now = new Date()
   selectedYear = signal<number>(this.now.getFullYear())
+  months: string[] = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"]
   // yearOptionsLength = this.now.getFullYear() - 2020
   // yearOptions = Array.from({ length: this.yearOptionsLength }, (_, i) => 2020 + i);
   ownerAppartStatistics = computed<AppartmentBusinessStat[]>(() => this.ownerAppartments().map(appart => appart.calculateBusinessStatistics()))
   yearOptions = computed(() => {
-    const allYears: number[] = []
+    const allYears: string[] = []
     this.ownerAppartStatistics().forEach(stat => {
       const years = stat.yearStatistics.map(item => item.year)
       years.forEach(year =>{
-        allYears.push(year)
+        allYears.push(year.toString())
       })
     })
 
-    return new Set(allYears.sort((a,b) => a - b))
+    return new Set(allYears.sort((a,b) => Number(a) - Number(b)))
   })
 
   constructor(
@@ -57,6 +60,29 @@ export class StatistiquesResaComponent implements OnInit {
         this.router.navigate(["/"])
       }
     }
+  }
+
+  getMonthlyEarns(row: AppartmentBusinessStat, monthIndex: number): number {
+    const yearStat = row.yearStatistics.find(item => item.year == this.selectedYear())
+    return yearStat ? yearStat.monthlyEarns[monthIndex] : 0
+  }
+
+  getYearTotal(row: AppartmentBusinessStat): number {
+    const yearStat = row.yearStatistics.find(item => item.year == this.selectedYear())
+    return yearStat ? yearStat.yearTotal : 0
+  }
+
+  getMonthlyTotal(monthIndex: number): number {
+    return this.ownerAppartStatistics().reduce((total, row) => {
+      const yearStat = row.yearStatistics.find(item => item.year == this.selectedYear());
+      return total + (yearStat ? yearStat.monthlyEarns[monthIndex] : 0)
+    }, 0)
+  }
+
+  getYearTotalForAll(): number {
+    return this.ownerAppartStatistics().reduce((total, row) => {
+      return total + this.getYearTotal(row)
+    }, 0)
   }
 
   clickMe(): void {
