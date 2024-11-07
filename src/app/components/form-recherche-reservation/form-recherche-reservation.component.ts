9af90cbae6ac1b05e25155446717c0cb9dc7990d
Jerome Desouches <jerome.desouches@gmail.com>
2024-11-07 13:22:00 +0100

    formulaire de recherche reservation

---
+++ src/app/components/form-recherche-reservation/form-recherche-reservation.component.ts
@@ -0,0 +1,122 @@
+import { ChangeDetectionStrategy, Component, computed, input, OnDestroy, OnInit, output } from '@angular/core';
+import { Appartment } from '../../models/Appartment.model';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Owner } from '../../models/Owner.model';
+import { ReservationResearchRequest } from '../../models/Request/ReservationResearchRequest.model';
+import { Subject, takeUntil } from 'rxjs';
+import { ReactiveInputComponent } from '../../shared/library/reactive-input/reactive-input.component';
+import { ReactiveSelectComponent } from '../../shared/library/reactive-select/reactive-select.component';
+import { SomeFunctionsService } from '../../shared/some-functions.service';
+
+@Component({
+  selector: 'app-form-recherche-reservation',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, ReactiveInputComponent, ReactiveSelectComponent],
+  templateUrl: './form-recherche-reservation.component.html',
+  styleUrl: './form-recherche-reservation.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FormRechercheReservationComponent implements OnInit, OnDestroy {
+
+  constructor(private readonly fb: FormBuilder, private readonly someFunctions: SomeFunctionsService){}
+
+  owners = input<Owner[] | null>()
+  appartments = input.required<Appartment[]>()
+
+  ownersOptions = computed(() => {
+    if(this.owners()){
+      const options: {value: number | null, label: string}[] = []
+      options.push({value: null, label: "---"})
+    this.owners()!.forEach(item => {
+      options.push({value: item.id, label: `${item.firstname} ${item.lastname}`
+      })
+    })
+    return options
+    } else {return []}
+  })
+
+  appartmentsOptions = computed(() => {
+    const options: {value: number | null, label: string}[] = []
+    options.push({value: null, label: "---"})
+    this.appartments()!.forEach(item => {
+      options.push({value: item.id, label: `${item.name}`
+      })
+    })
+    return options
+  })
+
+  yearsOptions: {value: number | null, label: string}[] = [] 
+  monthOptions: {value: number | null, label: string}[] = [] 
+  stateOptions: {value: "accepted" | "notAccepted" | null, label: string}[] = [
+    {value: null, label: "Toutes"},
+    {value: "accepted", label: "Acceptée"},
+    {value: "notAccepted", label: "Pas encore acceptée"}
+  ]
+  notClosedOptions: {value: boolean, label: string}[] = [
+    {value: false, label: "Non"},
+    {value: true, label: "Oui"},
+  ]
+
+  researchEmitter = output<ReservationResearchRequest>()
+
+  formResearch: FormGroup = new FormGroup({})
+  destroy$: Subject<void> = new Subject()
+
+  ngOnInit(): void {
+    this.yearsOptions = this.someFunctions.getYearsSelectOptions()
+    this.monthOptions = this.someFunctions.getMonthSelectOptions()
+      this.initForm()
+      this.initEvents()
+  }
+
+  initForm(): void {
+    this.formResearch = this.fb.group({
+      ownerId: new FormControl<number | null>(null),
+      appartmentId: new FormControl<number | null>(null),
+      year: new FormControl<number | null>(null),
+      month: new FormControl<number | null>(null),
+      state: new FormControl<"accepted" | "notAccepted" | null>(null),
+      notClosed: new FormControl<boolean>(false),
+      //TODO plateform (quand ajouté dans objet reservation...)
+    })
+
+    this.formResearch.get("month")?.disable()
+  }
+
+  initEvents(): void {
+    this.formResearch.get("year")?.valueChanges.pipe(takeUntil(this.destroy$)).subscribe((value) => {
+      if(value === "null") {value = null}
+
+      if(value) {
+        this.formResearch.get("month")?.enable()
+      } else {
+        this.formResearch.get("month")?.setValue(null)
+        this.formResearch.get("month")?.disable()
+      }
+    })
+  }
+  
+  
+
+  resetForm(): void {
+    this.formResearch.patchValue({
+      ownerId: null,
+      appartmentId: null,
+      year: null,
+      month: null,
+      state: null,
+      notClosed: false
+    })
+  }
+
+  onSubmit(): void {
+    this.researchEmitter.emit(this.formResearch.getRawValue())
+    this.resetForm()
+  }
+
+  ngOnDestroy(): void {
+      this.destroy$.next()
+      this.destroy$.complete()
+  }
+}
--- src/app/pages/admin-interface/synthese-resa/synthese-resa.component.ts
+++ src/app/pages/admin-interface/synthese-resa/synthese-resa.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { AppstoreService } from '../../../shared/appstore.service';
+import { AppartmentsService } from '../../../shared/appartments.service';
+import { NotificationService } from '../../../shared/notification.service';
+import { UtilisateurService } from '../../../shared/utilisateur.service';
+import { Router } from '@angular/router';
+import { User } from '../../../models/User.model';
+import { Appartment } from '../../../models/Appartment.model';
+import { Owner } from '../../../models/Owner.model';
+import { take } from 'rxjs';
+import { FormRechercheReservationComponent } from '../../../components/form-recherche-reservation/form-recherche-reservation.component';
 
 @Component({
   selector: 'app-synthese-resa',
   standalone: true,
-  imports: [],
+  imports: [FormRechercheReservationComponent],
   templateUrl: './synthese-resa.component.html',
   styleUrl: './synthese-resa.component.scss'
 })
-export class SyntheseResaComponent {
+export class SyntheseResaComponent implements OnInit {
+
+  constructor(
+    private readonly appstore: AppstoreService, 
+    private readonly appartmentService: AppartmentsService, 
+    private readonly notificationService: NotificationService, 
+    private readonly utilisateurService: UtilisateurService,
+    private readonly router: Router){}
+
+    currentUser: WritableSignal<User | null> = this.appstore.getCurrentUser()
+    allAppartments: WritableSignal<Appartment[]> = this.appstore.getAllAppartments()
+    selectedAppartment = signal<Appartment>(this.allAppartments()[0])
+    owners: WritableSignal<Owner[]> = this.appstore.getOwners()
+
+
+    ngOnInit(): void {
+        this.getAppartments()
+        this.getOwners()
+    }
+
+  getAppartments(): void {    
+    if(this.allAppartments().length === 0) {
+      if(this.currentUser()?.role === "ADMIN") {
+        this.appartmentService.getAllAppartments().pipe(take(1)).subscribe(
+          {
+            next: () => {
+              this.selectedAppartment.set(this.allAppartments()[0])
+            },
+            error: (error) => this.notificationService.error(error)
+          }
+        )
+      } else if (this.currentUser()?.role === "OWNER") {
+        this.appartmentService.getAppartmentsByOwnerId(this.currentUser()?.id!).pipe(take(1)).subscribe(
+          {
+            next: () => {this.selectedAppartment.set(this.allAppartments()[0])},
+            error: (error) => this.notificationService.error(error)
+          }
+        )
+      }
+    }
+  }
+
+  getOwners(): void {
+    if(this.currentUser()?.role === "ADMIN") {
+      this.utilisateurService.getAllOwners().pipe(take(1)).subscribe()
+    }
+  }
 
 }
